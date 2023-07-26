fd01489ada149d4e1d11a51547a4a6fa151450fa
me990928 <me990928@gmail.com>
2023-07-26 16:33:26 +0900

    add login UiState & bind

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/login/LoginUiState.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/ui/login/LoginUiState.kt
@@ -1,6 +1,24 @@
 package com.dmm.bootcamp.yatter2023.ui.login
 
-data class LoginUiState (
+import com.dmm.bootcamp.yatter2023.domain.model.Password
+import com.dmm.bootcamp.yatter2023.domain.model.Username
+
+data class LoginUiState(
     val loginBindingModel: LoginBindingModel,
-    val isLoading: Boolean
+    val isLoading: Boolean,
+    val validUsername: Boolean,
+    val validPassword: Boolean,
+) {
+    val isEnableLogin: Boolean = validUsername && validPassword
+    companion object {
+        fun empty(): LoginUiState = LoginUiState(
+            loginBindingModel = LoginBindingModel(
+                username = "",
+                password = ""
+            ),
+            validUsername = false,
+            validPassword = false,
+            isLoading = false,
         )
+    }
+}
